fe65de10e712821384de363518c56bf7ce77d395
Olalekan Yusuf <olasyusuf@gmail.com>
2025-08-31 03:32:58 +0100

    Cache Property List View - update

--- properties/views.py
+++ properties/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.core.cache import cache
-from django.shortcuts import render
+from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 
 from .models import Property
+from properties.serializers import PropertySerializer
 
 @cache_page(60 * 15)  # Cache the page for 15 minutes
 def property_list(request):
@@ -13,5 +13,8 @@ def property_list(request):
     Retrieves and displays a list of all properties.
     The entire page response is cached in Redis.
     """
-    properties = Property.objects.all()
-    return render(request, 'properties/property_list.html', {'properties': properties})
+    if request.method == "GET":
+        properties = Property.objects.all()
+        serializer = PropertySerializer(properties, many=True)
+        return JsonResponse({"properties": serializer.data}, safe=False)
+    return JsonResponse({"error": "Method not allowed"}, status=405)
